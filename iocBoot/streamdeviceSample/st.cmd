079f2c1a3644c56242330527b7572de60a59c231
Andrea Michelotti <andrea.michelotti@lnf.infn.it>
2026-03-20 14:14:23 +0100

    renamed into tti

--- iocBoot/streamdeviceSample/st.cmd
+++ iocBoot/streamdeviceSample/st.cmd
@@ -3,27 +3,15 @@
 < envPaths
 
 
-epicsEnvSet("IOC","streamdeviceSample")
-epicsEnvSet("TOP","/app/TTI-magnets-")
-epicsEnvSet("EPICS_BASE","/epics/epics-base")
-epicsEnvSet("ASYN","/epics/support/asyn")
-epicsEnvSet("CALC","/epics/support/calc")
-epicsEnvSet("SUPPORT","/epics/support")
-epicsEnvSet("STREAMDEVICE","/epics/support/StreamDevice")
-
 epicsEnvSet("STREAM_PROTOCOL_PATH", "$(TOP)/db")
 
-cd "$(TOP)"
-
-dbLoadDatabase "dbd/streamdeviceSample.dbd"
+dbLoadDatabase "$(TOP)/dbd/streamdeviceSample.dbd"
 streamdeviceSample_registerRecordDeviceDriver pdbbase
 
 drvAsynIPPortConfigure("TTI1","192.168.197.49:4001", 0, 0, 0)
 asynSetOption("TTI1", 0, "disconnectOnReadTimeout", "Y")
 
-dbLoadRecords("db/TTI.template", "P=SPARC:,R=TTI:,PORT=TTI1")
-
-cd "$(TOP)/iocBoot/$(IOC)"
+dbLoadRecords("$(TOP)/db/TTI.template", "P=SPARC:,R=TTI:,PORT=TTI1")
 
 iocInit
 epicsThreadSleep 1
